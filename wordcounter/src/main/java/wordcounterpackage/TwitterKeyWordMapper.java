6de3c13111129905b8f513c97bddb5fec8211248
Renier Velazco <reniervelazco@Reniers-MacBook-Pro.local>
2018-10-15 22:38:34 -0400

    Project implemented, next step is to run it with hadoop as a jar with the tweets as input files; to get an output folder with the results

--- wordcounter/src/main/java/wordcounterpackage/TwitterKeyWordMapper.java
+++ wordcounter/src/main/java/wordcounterpackage/TwitterKeyWordMapper.java
@@ -17,10 +17,23 @@ public class TwitterKeyWordMapper extends Mapper<LongWritable, Text, Text, IntWr
         try {
             Status status = TwitterObjectFactory.createStatus(rawTweet);
             String tweet = status.getText().toUpperCase();
-            if (tweet.contains("MAGA")) {
-                context.write(new Text("MAGA"), new IntWritable(1));
-            } else if (tweet.contains("DICTATOR")) {
-                context.write(new Text("DICTATOR"), new IntWritable(1));
+            if (tweet.contains("Trump")) {
+                context.write(new Text("Trump"), new IntWritable(1));
+            }
+            else if (tweet.contains("Zika")) {
+                context.write(new Text("Zika"), new IntWritable(1));
+            }
+            else if (tweet.contains("Diarrhea")) {
+                context.write(new Text("Diarrhea"), new IntWritable(1));
+            }
+            else if (tweet.contains("Ebola")) {
+                context.write(new Text("Diarrhea"), new IntWritable(1));
+            }
+            else if (tweet.contains("Headache")) {
+                context.write(new Text("Headache"), new IntWritable(1));
+            }
+            else if (tweet.contains("Measles")) {
+                context.write(new Text("Measles"), new IntWritable(1));
             }
         } catch (TwitterException e) {
 
